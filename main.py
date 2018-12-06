f5936b30c2a5bee02e107a12631cefc9f82ca2a1
LeonLIU <lmyleon2014@gmail.com>
2018-12-06 20:05:21 +0800

    add resutls

--- main.py
+++ main.py
@@ -4,6 +4,16 @@ import torch
 
 from mdn import MDN
 
+# global variables
+n_samples = 1000
+tes_samples = 1000
+n_gaussian = 5
+
+def singleGaussian(x, mu, sigma):
+    a = (2*np.pi*sigma)**0.5
+    b = np.exp(-1*(x-mu)**2/(2*sigma))
+    return b/a
+
 
 def mdn_loss(y, mu, sigma, pi):
     m = torch.distributions.Normal(loc=mu, scale=sigma)
@@ -15,8 +25,7 @@ def mdn_loss(y, mu, sigma, pi):
 
 def main():
     # paraper the data
-    n_samples = 1000
-    tes_samples = 1000
+
     epsilon = torch.randn(n_samples)
     x_data = torch.linspace(-10, 10, n_samples)
     y_data = 7 * torch.sin(0.75 * x_data) + 0.5 * x_data + 1.*epsilon
@@ -25,20 +34,24 @@ def main():
 
     x_test = torch.linspace(-15, 15, tes_samples).view(-1, 1)
 
-    model = MDN(n_hidden=20, n_gaussian=5)
+    model = MDN(n_hidden=20, n_gaussian=n_gaussian)
     model.train()
 
     optimizer = torch.optim.Adam(model.parameters())
 
-    for epoch in range(10000):
+    for epoch in range(15000):
         pi, mu, sigma = model(x_data)
         loss = mdn_loss(y_data, mu, sigma, pi)
         optimizer.zero_grad()
         loss.backward()
         optimizer.step()
-        if epoch % 1000 == 0:
-            print(loss.data.tolist())
+        if epoch % 500 == 0:
+            print(epoch, loss.data.tolist())
+            validate(model, x_test, x_data, y_data, epoch//1000,
+                     savefig=True)
 
+
+def validate(model, x_test, x_data, y_data, epoch, savefig=False):
     model.eval()
     pi, mu, sigma = model(x_test)
     # print(pi, mu, sigma)
@@ -46,43 +59,61 @@ def main():
     k = torch.multinomial(pi, 1).view(-1)  # select one with probability
     y_pred = torch.normal(mu, sigma)[np.arange(tes_samples), k].data
     # print(k)
-    plt.figure(figsize=(14, 8))
-    plt.subplot(121)
+
+    # plot the figures
+    color_list = ['r', 'b', 'g', 'k', 'y']
+    plt.figure(figsize=(24, 14))
+    plt.subplot(231)
+    plt.scatter(x_data, y_data, alpha=0.4)
+    plt.xlabel('Training Data', fontsize=15)
+
+    plt.subplot(232)
     plt.scatter(x_data, y_data, alpha=0.4)
     plt.scatter(x_test, y_pred, alpha=0.4, color='red')
-    plt.subplot(122)
-    for i in range(5):
-        plt.plot(pi.data.numpy()[:, i])
+    plt.xlabel('Predicted Results', fontsize=15)
 
-    plt.show()
+    plt.subplot(233)
+    for i in range(n_gaussian):
+        plt.plot(30.*np.arange(tes_samples)/float(tes_samples)-15.,
+                 pi.data.numpy()[:, i], color=color_list[i])
+    plt.xlabel(r'$\alpha(i)$', fontsize=15)
 
-    # run it again
-    pi, mu, sigma = model(x_test)
-    # print(pi, mu, sigma)
+    xx = np.linspace(-15, 15, n_samples)
 
-    k = torch.multinomial(pi, 1).view(-1)  # select one with probability
-    y_pred = torch.normal(mu, sigma)[np.arange(tes_samples), k].data
-    # print(k)
-    plt.figure(figsize=(14, 8))
-    plt.subplot(121)
-    plt.scatter(x_data, y_data, alpha=0.4)
-    plt.scatter(x_test, y_pred, alpha=0.4, color='red')
-    plt.subplot(122)
-    for i in range(5):
-        plt.plot(pi.data.numpy()[:, i])
+    plt.subplot(234)
+    pi, mu, sigma = model(torch.Tensor([-10.]))
+    for i in range(n_gaussian):
+        p = pi.data.numpy()[i]
+        m = mu.data.numpy()[i]
+        s = sigma.data.numpy()[i]
+        out = singleGaussian(xx, m, s)
+        plt.plot(xx, out, color=color_list[i])
+    plt.xlabel('x=-10', fontsize=15)
 
-    plt.show()
+    plt.subplot(235)
+    pi, mu, sigma = model(torch.Tensor([0.]))
+    for i in range(n_gaussian):
+        p = pi.data.numpy()[i]
+        m = mu.data.numpy()[i]
+        s = sigma.data.numpy()[i]
+        out = singleGaussian(xx, m, s)
+        plt.plot(xx, out, color=color_list[i])
+    plt.xlabel('x=0', fontsize=15)
 
+    plt.subplot(236)
+    pi, mu, sigma = model(torch.Tensor([10.]))
+    for i in range(n_gaussian):
+        p = pi.data.numpy()[i]
+        m = mu.data.numpy()[i]
+        s = sigma.data.numpy()[i]
+        out = singleGaussian(xx, m, s)
+        plt.plot(xx, out, color=color_list[i])
+    plt.xlabel('x=10', fontsize=15)
 
-
-
-
-
-
-
-
-
-
+    plt.tight_layout()
+    if savefig:
+        plt.savefig('epoch:%dk' % epoch, dpi=300)
+    # plt.show()
 
 
 
